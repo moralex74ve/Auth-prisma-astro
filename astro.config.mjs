54cbcbde2bc9412b2908606b788df432a7b9fbfc
moralex74ve <moralex74@gmail.com>
2025-09-14 14:23:23 -0400

    Respaldo: Punto de guardado antes de refactorizar la autenticación

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,10 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
-// import node from '@astrojs/node';
-// import { server } from './src/actions';
-
-// https://astro.build/config
 export default defineConfig({
   output: "static",
   integrations: [tailwind()],
   adapter: node({
     mode: "standalone",
   }),
-  experimental: {
-    // @ts-ignore
-    session: {
-      driver: "fs",
-      // If set to a string, this will be used as the cookie name
-      // cookie: "my-session-id",
-      // If set to an object, this will allow advanced options to be set
-      cookie: {
-        name: "my-session-id",
-        sameSite: "none",
-      },
-    },
-  },
 });
