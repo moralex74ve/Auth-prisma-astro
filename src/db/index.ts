138fef54f7aa685e9637581c3b08dba12cf44399
moralex74ve <moralex74@gmail.com>
2025-09-17 10:27:19 -0400

    Actualizacion de segurridad y refuerzo ante fuerz bruta

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient();
-// use `prisma` in your application to read and write data in your DB
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
